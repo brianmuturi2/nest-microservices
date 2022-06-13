268c7dbe30df4671dfd8d6d54815b3732e212c64
brianmuturi2 <brianmuturi2@gmail.com>
2022-06-13 16:41:19 +0300

    feat(client): implemented post endpoint and configured tcp microservice

--- apps/client/src/app/app.controller.ts
+++ apps/client/src/app/app.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
-@Controller()
+@Controller('add')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              @Inject('MATH_SERVICE') private client: ClientProxy) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post()
+  accumulate(@Body('data') data: any): Observable<number> {
+    return this.client.send<number, number[]>('add', data);
   }
 }
